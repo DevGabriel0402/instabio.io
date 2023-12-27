31349d2d031a3fc513ed3cb037d7c191e220d84d
DevGabriel0402 <136494625+DevGabriel0402@users.noreply.github.com>
2023-12-26 23:06:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 new WOW().init() 
+
+function botaowhatsApp() {
+
+    texto = 'Olá, gostaria de *fazer um orçamento* com você Gabriel, vim do instagram.'
+    encode = encodeURI(texto)
+
+    url = `https://wa.me/5531983272409?text=` + encode
+
+
+
+    $('#btn-whatsapp').attr('href', url)
+}
+
+botaowhatsApp()
